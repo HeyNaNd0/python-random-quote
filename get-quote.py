d275d183c5b3fb40c56d6baa20aab83d4eb0045b
NaNdo <eric.fernandez4@gmail.com>
2022-05-14 22:20:38 -0500

    Updated spaces form 4 to 2

--- get-quote.py
+++ get-quote.py
@@ -2,14 +2,12 @@ import random
 
 def logic():
   print("Keep it logically awesome.")
-
-
-f = open("quotes.txt")
-quotes = f.readlines()
-f.close()
-last = 13
-rnd = random.randint(0,last)
-print(quotes[rnd]+"\n"+ quotes[rnd-1])
+  f = open("quotes.txt")
+  quotes = f.readlines()
+  f.close()
+  last = 13
+  rnd = random.randint(0,last)
+  print(quotes[rnd]+"\n"+ quotes[rnd-1])
 
 if __name__ == "__main__":
   logic()
